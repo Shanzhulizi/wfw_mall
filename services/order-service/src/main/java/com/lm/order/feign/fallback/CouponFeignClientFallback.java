1fb1e9d65ebd44d3e727c3cff62131115a5ccbea
LMCow <2863993244@qq.com>
2025-07-18 18:59:53 +0800

    基本完成下单流程，接下来进行优惠券操作

--- services/order-service/src/main/java/com/lm/order/feign/fallback/CouponFeignClientFallback.java
+++ services/order-service/src/main/java/com/lm/order/feign/fallback/CouponFeignClientFallback.java
@@ -1,6 +1,14 @@
 package com.lm.order.feign.fallback;
 
+import com.lm.common.R;
+import com.lm.order.dto.CouponUseDTO;
 import com.lm.order.feign.CouponFeignClient;
 
+import java.util.List;
+
 public class CouponFeignClientFallback implements CouponFeignClient {
+    @Override
+    public R lockCoupon(List<CouponUseDTO> coupons, Long userId) {
+        return null;
+    }
 }
